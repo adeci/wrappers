5ae571324a9ba8e2976fcc10927f27f5623ffcee
adeci <alex.decious@gmail.com>
2025-11-25 18:30:48 +0700

    radically simplified fish

--- modules/fish/check.nix
+++ modules/fish/check.nix
@@ -9,46 +9,48 @@ let
       inherit pkgs;
 
       "config.fish".content = ''
-        # Test configuration
+        # Test configuration that loads after system defaults
         set -gx TEST_VAR "test_value"
-        set fish_greeting "Test Fish Shell"
+        set -g fish_greeting "Test Fish Shell"
 
-        # Add an alias for testing
+        # Test alias
         alias test-alias='echo "Alias works!"'
-      '';
 
-      functions = {
-        greet = ''
+        # Test inline function
+        function greet
           echo "Hello, $argv"
-        '';
+        end
 
-        testfunc = ''
+        function testfunc
           echo "Test function works!"
-        '';
-      };
-
-      "conf.d" = {
-        "01-test" = ''
-          # This runs before config.fish
-          set -gx CONF_D_TEST "loaded"
-        '';
-      };
+        end
+      '';
 
     }).wrapper;
 
 in
 pkgs.runCommand "fish-test" { } ''
+  # Test 1: Fish version works
   "${fishWrapped}/bin/fish" --version | grep -q "fish"
 
+  # Test 2: Inline functions work
   "${fishWrapped}/bin/fish" -c "greet World" | grep -q "Hello, World"
-
   "${fishWrapped}/bin/fish" -c "testfunc" | grep -q "Test function works!"
 
+  # Test 3: Config variables are set
   "${fishWrapped}/bin/fish" -c 'echo $TEST_VAR' | grep -q "test_value"
 
-  "${fishWrapped}/bin/fish" -c 'echo $CONF_D_TEST' | grep -q "loaded"
-
+  # Test 4: Aliases work
   "${fishWrapped}/bin/fish" -c "test-alias" | grep -q "Alias works!"
+
+  # Test 5: System integration - basic fish functions should be available
+  "${fishWrapped}/bin/fish" -c "type -q fish_add_path" && echo "System functions loaded"
+
+  # Test 6: Completions are available (check for basic cd completion function)
+  "${fishWrapped}/bin/fish" -c "functions -q __fish_complete_cd" && echo "System completions loaded"
+
+  # Test 7: Our config loads AFTER system (test that we can override)
+  "${fishWrapped}/bin/fish" -c 'echo $fish_greeting' | grep -q "Test Fish Shell"
 
   touch $out
 ''
--- modules/fish/module.nix
+++ modules/fish/module.nix
@@ -4,23 +4,6 @@
   wlib,
   ...
 }:
-let
-  # Generate function files from the functions attrset
-  functionFiles = lib.mapAttrsToList (name: content: {
-    name = "fish/functions/${name}.fish";
-    path = config.pkgs.writeText "${name}.fish" ''
-      function ${name}
-          ${content}
-      end
-    '';
-  }) config.functions;
-
-  # Generate conf.d snippet files if provided
-  confFiles = lib.mapAttrsToList (name: content: {
-    name = "fish/conf.d/${name}.fish";
-    path = config.pkgs.writeText "${name}.fish" content;
-  }) config."conf.d";
-in
 {
   _class = "wrapper";
 
@@ -30,10 +13,8 @@ in
       default.content = "";
       description = ''
         Your fish configuration file content.
-        Write as you would for ~/.config/fish/config.fish
-
-        Example:
-        ```fish
+      '';
+      example = ''
         set -gx EDITOR nvim
         set -gx PATH $PATH /usr/local/bin
 
@@ -41,92 +22,21 @@ in
         abbr gc 'git commit'
 
         set fish_greeting ""
-        ```
-      '';
-    };
 
-    functions = lib.mkOption {
-      type = lib.types.attrsOf lib.types.lines;
-      default = { };
-      description = ''
-        Fish functions. Each attribute becomes a function file in functions/.
-        The key is the function name, the value is the function body.
-
-        Example:
-        ```nix
-        functions = {
-          greet = '''
-            echo "Hello, $argv"
-          ''';
-          mkcd = '''
+        # Define functions inline
+        function mkcd
             mkdir -p $argv[1]
-            cd $argv[1]
-          ''';
-        };
-        ```
-      '';
-    };
-
-    "conf.d" = lib.mkOption {
-      type = lib.types.attrsOf lib.types.lines;
-      default = { };
-      description = ''
-        Configuration snippets that run before config.fish.
-        Useful for modular configuration or overridable defaults.
-        Files are executed in sorted order by filename.
-
-        Example:
-        ```nix
-        "conf.d" = {
-          "01-path" = '''
-            set -gx PATH /opt/homebrew/bin $PATH
-          ''';
-          "02-colors" = '''
-            set -gx CLICOLOR 1
-          ''';
-        };
-        ```
+            and cd $argv[1]
+        end
       '';
     };
   };
 
-  config.args =
-    let
-      # Create a directory with our functions
-      functionsDir =
-        if functionFiles != [ ] then config.pkgs.linkFarm "fish-functions" functionFiles else null;
-
-      # Create a directory with our conf.d files
-      confDir = if confFiles != [ ] then config.pkgs.linkFarm "fish-conf.d" confFiles else null;
-
-      # Create the init script that sets up our environment
-      initScript = config.pkgs.writeText "fish-init.fish" ''
-        ${lib.optionalString (functionsDir != null) ''
-          # Add our functions to the function path
-          set -p fish_function_path "${functionsDir}/fish/functions"
-        ''}
-
-        ${lib.optionalString (confDir != null) ''
-          # Source conf.d files in order
-          for conf in ${confDir}/fish/conf.d/*.fish
-            source $conf
-          end
-        ''}
-
-        # Source our main config
-        ${lib.optionalString (config."config.fish".content != "") ''
-          source "${config."config.fish".path}"
-        ''}
-      '';
-    in
-    [
-      # Totally isolated from system and default fish configs
-      # This means we must set EVERYTHING ourselves
-      # Normal fish integrations will not work unless explicitly set
-      "--no-config"
-      "--init-command"
-      "source ${initScript}"
-    ];
+  config.args = lib.optionals (config."config.fish".content != "") [
+    # Use system fish config and all integrations
+    "--init-command"
+    "source ${config."config.fish".path}"
+  ];
 
   config.package = config.pkgs.fish;
 
